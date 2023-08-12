e84d777ef3ea133679cee54f37039553cbc1d7da
lhouarhou <l.ouarhou.ensa@gmail.com>
2023-08-12 18:11:25 +0100

    feat(features): integrate features flag into the app entry point

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { FeatureToggle } from "../components/FeatureToggle";
 import "../styles/globals.css";
+import { fetchFeatures } from "../services/FeatureToggle";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [enabledFeatures, setFeatures] = useState<string[]>([]);
+
+  const processFeatures = async () => {
+    const features = await fetchFeatures();
+
+    setFeatures(features);
+  };
+
+  useEffect(() => {
+    processFeatures();
+  }, []);
+
+  return (
+    <FeatureToggle enabledFeatures={enabledFeatures}>
+      <Component {...pageProps} />
+    </FeatureToggle>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,23 @@ import Head from "next/head";
 import { Layout } from "antd";
 
 import { GdpChart, TreasuryChart } from "../components/Charts";
+import { useFeatureToggle } from "../hooks/useFeatureToggle";
 
 const { Header, Content } = Layout;
 
 export default function Home() {
+  const [isEnabled] = useFeatureToggle();
   return (
     <Layout className="layout">
       <Head>
-        <title>🚦 Feature Toggle in Next.js</title>
+        <title> Feature Toggle in Next.js</title>
       </Head>
       <Header>
         <div className="logo" />
       </Header>
       <Content className="content">
         <GdpChart />
-        <TreasuryChart />
+        {isEnabled("treasury_chart") && <TreasuryChart />}
       </Content>
     </Layout>
   );
